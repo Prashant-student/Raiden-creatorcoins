93716f7eabf41628b280ea338b7de0110486d2d3
Pavan2601 <pavan.kulkarni@somaiya.edu>
2021-08-23 15:01:49 +0530

    done

--- src/Components/Pages/SignUp.tsx
+++ src/Components/Pages/SignUp.tsx
@@ -10,7 +10,15 @@ const SignUp = () => {
         height: '90vh',
       }}
     >
-      <h1>Sign Up</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+          <br />
+          Last Name:
+          <input type="text" name="LastName" />
+        </label>
+      </form>
     </div>
   );
 };
